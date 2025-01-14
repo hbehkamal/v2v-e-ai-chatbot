bbe175dfdf5c4b5287143218f36d42984021bb71
Hosein Behkamal <hoseynbehkamal@gmail.com>
2025-01-14 21:37:37 +0330

    chore: implement Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+const Loading = () => (
+  <div className="flex justify-center mt-4">
+    <div className="loader bg-blue-500 text-white p-2 rounded-full w-6 h-6 animate-pulse"></div>
+  </div>
+);
+
+export default Loading;
